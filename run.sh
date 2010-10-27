6a42d5ef07dd454934d5e4c9e4d3909890bcd4a7
Nicolas Stinus <nicolas.stinus@gmail.com>
2010-10-27 19:25:08 +0200

    [batch] human readable date in log filename

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 GIT_COMMIT=$(git describe --tags --abbrev --dirty)
 BIN=$(pwd)/gen-py/dino.py
-LOG=/tmp/dino.${GIT_COMMIT}.$(date +%s)
+LOG=/tmp/dino.${GIT_COMMIT}.$(date +'%Y%m%d_%H%M%S').log
 
 python $BIN 2>&1 | tee $LOG
