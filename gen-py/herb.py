bc2759118becd02f38a9e2d58b030b3835c0c5b8
Nicolas Stinus <nicolas.stinus@gmail.com>
2010-10-28 22:17:57 +0200

    [refactor] extracted MapManager into own module dino.mngr

--- gen-py/herb.py
+++ gen-py/herb.py
@@ -17,7 +17,8 @@ from dinoisland.ttypes import GameOverException, YouAreDeadException, BadEggExce
 from thrift.transport.TSocket import TSocket
 from thrift.protocol.TBinaryProtocol import TBinaryProtocol
 
-from dino.core import vectorToDirections, vectorToOrientation, minmax, getCone, counter
+from dino.core import vectorToDirections, minmax, getCone, counter
+from dino.mngr import MapManager
 
 import logging
 LOG_FORMAT = "%(asctime)s  %(name)-10s  %(levelname)-8s  %(message)s"
@@ -39,60 +40,6 @@ DINO_COUNTER = counter(0)
 
 __raw_git_desc = getstatusoutput("git describe --tags --long --abbrev --dirty")
 GIT_DESCRIBE = __raw_git_desc[0] == 0 and __raw_git_desc[1] or None
-
-class MapManager:
-    def __init__(self):
-        self.sightings = list()
-        self.lock = threading.Lock()
-        self.logger = logging.getLogger("Map")
-
-    def __addSighting(self, sighting, position):
-        sighting.coordinate = sighting.coordinate.toAbsolute(position)
-        self.sightings.append(sighting)
-
-    def __deleteSightings(self, position, direction, distance):
-        old_n = len(self.sightings)
-        if len(self.sightings) != 0:
-            self.sightings = [i for i in self.sightings \
-                              if i.coordinate.distance(position) > distance \
-                                  or vectorToOrientation(i.coordinate - position) \
-                                      not in getCone(direction)]
-        new_n = len(self.sightings)
-        msg = "DELETE pos=%s, dir=%s, d=%d: deleted %d/%d (is %d now) sightings"
-        self.logger.info(msg \
-            % (position,
-               Direction._VALUES_TO_NAMES[direction],
-               distance,
-               old_n - new_n,
-               old_n,
-               new_n))
-
-    def addSightings(self, position, direction, distance, sightings):
-        self.lock.acquire()
-        self.__deleteSightings(position, direction, distance)
-        old_n = len(self.sightings)
-        for s in sightings:
-            self.__addSighting(s, position)
-        new_n = len(self.sightings)
-        self.lock.release()
-        msg = "ADD pos=%s: added %d/%d (is %d now) sightings"
-        self.logger.info(msg \
-            % (position, new_n - old_n, old_n, new_n))
-
-    def findClosest(self, position, type):
-        """ Returns the list of closest elements reachable from my current position.
-            Returned positions are absolute. """
-        self.lock.acquire()
-        self.sightings = [i for i in self.sightings if i.coordinate != position]
-        l = sorted([deepcopy(i).alterCoordsToRelative(position) \
-                    for i in self.sightings if i.type == type])
-        self.lock.release()
-        l = [deepcopy(i).alterCoordsToAbsolute(position) for i in l]
-        if len(l) > 0:
-            for i in l:
-                self.logger.debug("Closest elements: %s" % i)
-            return l
-        return None
 
 
 class Dino(Dinosaur.Client, threading.Thread):
